ee3ed22377907dd41fa0c49fed04ab8caaec31d8
Benjamin Li <34200168+liben002@users.noreply.github.com>
2021-11-01 19:39:26 -0400

    Create install_openmpi.sh

---
+++ centos/install_openmpi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e # exit when any error occurs
+
+# Install dependencies
+sudo yum install gcc-gfortran -y
+
+install_dir="/usr/local"
+
+echo "=========================="
+echo "Installing OpenMPI to $install_dir"
+echo "=========================="
+
+cd "$install_dir"
+sudo mkdir -p openmpi
+cd src
+sudo wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.1.tar.gz
+sudo tar -xvzf openmpi-4.1.1.tar.gz
+sudo rm openmpi-4.1.1.tar.gz
+cd openmpi-4.1.1
+sudo ./configure --prefix="$install_dir"/openmpi
+
+# Change to 'make all install -j{cores}' to build in parallel
+sudo make all install -j4
+
+if [ -z "$(ls -A ../../openmpi)" ]; then
+    echo "MPI did not install correctly, directory $install_dir/openmpi is empty."
+    exit 1
+else
+   echo "========================"
+   echo "MPI installed correctly. Make sure to add the line"
+   echo "export PATH=\"\$PATH:/usr/local/openmpi/bin\""
+   echo "to each user's .bashrc"
+   echo "========================"
+   echo ""
+   echo "========================"
+   echo "Look at the README for instructions on running mpiexec/mpirun commands."
+   echo "========================"
+fi
