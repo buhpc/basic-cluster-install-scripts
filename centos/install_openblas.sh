a58fdef23392a0518994d89e531a5b38b3ee262d
Benjamin Li <34200168+liben002@users.noreply.github.com>
2021-11-01 19:36:30 -0400

    Create install_openblas.sh

---
+++ centos/install_openblas.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e # exit when any error occurs
+
+# Install dependencies
+
+install_dir="/usr/local"
+
+echo "=========================="
+echo "Installing OpenBLAS to $install_dir"
+echo "=========================="
+
+cd "$install_dir"
+sudo mkdir -p openblas
+cd src
+sudo wget https://github.com/xianyi/OpenBLAS/releases/download/v0.3.17/OpenBLAS-0.3.17.tar.gz
+sudo tar -xvzf OpenBLAS-0.3.17.tar.gz
+sudo rm OpenBLAS-0.3.17.tar.gz
+cd OpenBLAS-0.3.17
+
+# Change to 'sudo make -j{cores}' to build in parallel
+sudo make -j4
+
+sudo make PREFIX="$install_dir/openblas" install
+
+if [ -z "$(ls -A ../../openblas)" ]; then
+    echo "OpenBLAS did not install correctly, directory $install_dir/openblas is empty."
+    exit 1
+else
+   echo "============================="
+   echo "OpenBLAS installed correctly."
+   echo "============================="
+fi
