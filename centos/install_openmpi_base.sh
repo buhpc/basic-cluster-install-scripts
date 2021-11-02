eda6a320e00816a82b000990a86fa475082dabff
Benjamin Li <34200168+liben002@users.noreply.github.com>
2021-11-02 12:25:36 -0400

    Update and rename install_openmpi.sh to install_openmpi_base.sh

--- centos/install_openmpi_base.sh
+++ centos/install_openmpi_base.sh
@@ -11,24 +11,24 @@ echo "Installing OpenMPI to $install_dir"
 echo "=========================="
 
 cd "$install_dir"
-sudo mkdir -p openmpi src
+sudo mkdir -p openmpi-installation src
 cd src
 sudo wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.1.tar.gz
 sudo tar -xvzf openmpi-4.1.1.tar.gz
 sudo rm openmpi-4.1.1.tar.gz
 cd openmpi-4.1.1
-sudo ./configure --prefix="$install_dir"/openmpi
+sudo ./configure --prefix="$install_dir"/openmpi-installation
 
 # Change to 'make all install -j{cores}' to build in parallel
 sudo make all install -j4
 
-if [ -z "$(ls -A ../../openmpi)" ]; then
-    echo "MPI did not install correctly, directory $install_dir/openmpi is empty."
+if [ -z "$(ls -A ../../openmpi-installation/bin)" ]; then
+    echo "MPI did not install correctly, directory $install_dir/openmpi-installation/bin is empty."
     exit 1
 else
    echo "========================"
    echo "MPI installed correctly. Make sure to add the line"
-   echo "export PATH=\"\$PATH:/usr/local/openmpi/bin\""
+   echo "export PATH=\"\$PATH:/usr/local/openmpi-installation/bin\""
    echo "to each user's .bashrc"
    echo "========================"
    echo ""
