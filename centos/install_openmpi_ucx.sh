70091e370ff164f1c00d5156ccc3c6d3af1c31d0
Benjamin Li <34200168+liben002@users.noreply.github.com>
2021-11-04 11:29:17 -0400

    Update install_openmpi_ucx.sh

--- centos/install_openmpi_ucx.sh
+++ centos/install_openmpi_ucx.sh
@@ -9,7 +9,7 @@ sudo yum install gcc-gfortran -y
 install_dir="/usr/local"
 
 echo "=========================="
-echo "Installing OpenMPI to $install_dir"
+echo "Installing OpenMPI to ${install_dir}"
 echo "=========================="
 
 cd "$install_dir"
@@ -19,18 +19,18 @@ sudo wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.1.tar.
 sudo tar -xvzf openmpi-4.1.1.tar.gz
 sudo rm openmpi-4.1.1.tar.gz
 cd openmpi-4.1.1
-sudo ./configure --prefix="$install_dir/openmpi-installation" --with-ucx="$install_dir/openucx-installation" --enable-mca-no-build=btl-uct
+sudo ./configure --prefix="${install_dir}/openmpi-installation" --with-ucx="${install_dir}/openucx-installation" --enable-mca-no-build=btl-uct
 
 # Change to 'make all install -j{cores}' to build in parallel
 sudo make all install -j16
 
 if [ -z "$(ls -A ../../openmpi-installation/bin)" ]; then
-    echo "MPI did not install correctly, directory $install_dir/openmpi-installation/bin is empty."
+    echo "MPI did not install correctly, directory ${install_dir}/openmpi-installation/bin is empty."
     exit 1
 else
    echo "========================"
    echo "MPI installed correctly. Make sure to add the line"
-   echo "export PATH=\"\$PATH:/usr/local/openmpi-installation/bin\""
+   echo "export PATH=\"\${PATH}:/usr/local/openmpi-installation/bin\""
    echo "to each user's .bashrc"
    echo "========================"
    echo ""
