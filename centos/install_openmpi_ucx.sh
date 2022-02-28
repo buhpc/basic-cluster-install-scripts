f1658d588105b13975df7e225182549575e12e1c
Benjamin Li <34200168+liben002@users.noreply.github.com>
2022-02-27 23:57:20 -0500

    Update install_openmpi_ucx.sh

--- centos/install_openmpi_ucx.sh
+++ centos/install_openmpi_ucx.sh
@@ -12,29 +12,37 @@ echo "=========================="
 echo "Installing OpenMPI to ${install_dir}"
 echo "=========================="
 
-cd "$install_dir"
+cd $install_dir
 sudo mkdir -p openmpi-installation src
 cd src
 sudo wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.1.tar.gz
 sudo tar -xvzf openmpi-4.1.1.tar.gz
 sudo rm openmpi-4.1.1.tar.gz
 cd openmpi-4.1.1
-sudo ./configure --prefix="${install_dir}/openmpi-installation" --with-ucx="${install_dir}/openucx-installation" --enable-mca-no-build=btl-uct
+sudo ./configure --prefix=${install_dir}/openmpi-installation --with-ucx=${install_dir}/openucx-installation --enable-mca-no-build=btl-uct
 
 # Change to 'make all install -j{cores}' to build in parallel
-sudo make all install -j16
+sudo make -j4 all
+sudo make install
 
-if [ -z "$(ls -A ../../openmpi-installation/bin)" ]; then
-    echo "MPI did not install correctly, directory ${install_dir}/openmpi-installation/bin is empty."
-    exit 1
+# A correct installation of OpenMPI will contain executables in the bin directory
+DIR="${install_dir}/openmpi-installation/bin"
+if [ -d "$DIR" ]; then
+    echo "======================================================================="
+    echo "MPI installed correctly. Make sure to add the line"
+    echo ""
+    echo "export PATH=\"\$PATH:/usr/local/openmpi-installation/bin\""
+    echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:/usr/local/openmpi-installation/bin\""
+    echo ""
+    echo "to the user's .bashrc"
+    echo "======================================================================="
+    echo ""
+    echo "======================================================================="
+    echo "Look at the README for instructions on running mpiexec/mpirun commands."
+    echo "======================================================================="
 else
-   echo "========================"
-   echo "MPI installed correctly. Make sure to add the line"
-   echo "export PATH=\"\${PATH}:/usr/local/openmpi-installation/bin\""
-   echo "to each user's .bashrc"
-   echo "========================"
-   echo ""
-   echo "========================"
-   echo "Look at the README for instructions on running mpiexec/mpirun commands."
-   echo "========================"
+    echo "==========================================================="
+    echo "MPI did not install correctly."
+    echo "Directory ${install_dir}/openmpi-installation/bin is empty."
+    echo "==========================================================="
 fi
